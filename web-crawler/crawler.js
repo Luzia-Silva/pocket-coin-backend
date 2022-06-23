e4905159d308116142c364bc29f3b24216da1fb8
Luzia <aluziagabriela@gmail.com>
2022-06-23 16:04:08 -0300

    news opciones

--- web-crawler/crawler.js
+++ web-crawler/crawler.js
@@ -2,12 +2,13 @@ const axios = require("axios");
 const jssoup = require('@aghajari/jssoup')
 const Cache = require("node-cache");
 const cache = new Cache();
+const backupNews = require('./default')
 
 class News {
     async getNews(url) {
         let response = cache.get("crawler");
         if (response) {
-            return response;
+            // return response;
         }
         return this.refine(await this.html(url))
     }
@@ -19,13 +20,14 @@ class News {
 
     refine(html) {
         const response = [];
-        const news = jssoup.load(html).find('.sc-2f06e235-12 .eLXeLv');
+        const news = jssoup.load(html).find('.palmeiras .E');
         for (let i = 0; i < news.length; i++) {
             const content = news[i].findFirst('.touch-area')
             response.push({title: content.plainText(), link: 'https://exame.com' + content.getValue('href')})
         }
-        cache.set("crawler", response, 10000);
-        return response;
+        //cache.set("crawler", response, 10000);
+        return response.length > 0 ? response : backupNews; 
+
     }
 }
 
